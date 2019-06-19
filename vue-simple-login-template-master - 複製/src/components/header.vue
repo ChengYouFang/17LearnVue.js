53d9d1e6e35539d3e6b5e62b6d2e79f3919107db
x213212 <x8795278@gmail.com>
2019-06-19 17:38:27 +0800

    x213212
    預計增加ssl

---
+++ vue-simple-login-template-master - 複製/src/components/header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/userInfo">User Info</router-link>
+
+    <a href @click.prevent="logout">Logout</a>
+
+    <el-dropdown trigger="click" @command="handleCommand">
+      <span class="el-dropdown-link">
+        多國語系
+        <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item command="cn">chines</el-dropdown-item>
+        <el-dropdown-item command="en">english</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+<script>
+import { removeToken } from "../api/cookiesex";
+export default {
+  methods: {
+    logout() {
+      //localStorage.removeItem("token");
+      removeToken();
+      this.$router.push("/login");
+    },
+    handleCommand(command) {
+      this.$message("change lang " + command);
+      if (command == "en") this.$i18n.locale = "en";
+      else this.$i18n.locale = "cn";
+    }
+  }
+};
+</script>
---
+++ vue-simple-login-template-master - 複製/src/routes.js
@@ -0,0 +1,29 @@
+import Login from './components/login.vue'
+import Header from './components/header.vue'
+import Home from './components/table.vue'
+import UserInfo from './components/userInfo.vue'
+
+export const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    components: {
+      default: Home,
+      nav: Header
+    }
+  },
+  {
+    path: '/userInfo',
+    components: {
+      default: UserInfo,
+      nav: Header
+    }
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
+]
